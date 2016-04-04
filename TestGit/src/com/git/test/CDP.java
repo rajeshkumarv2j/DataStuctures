e3832c971f2a5a0b8f7efc1d6064f270622f5e91
Rajeshkumar <rajeshkumar.v2j@gmail.com>
2016-04-04 09:29:36 +0530

    Started Working on CDP UW

--- TestGit/src/com/git/test/CDP.java
+++ TestGit/src/com/git/test/CDP.java
@@ -1,5 +1,7 @@
 package com.git.test;
 
 public class CDP {
-	
+	public static void main(String[] args) {
+		System.out.println("In CDP");
+	}
 }
--- TestGit/src/com/git/test/UW.java
+++ TestGit/src/com/git/test/UW.java
@@ -1,5 +1,7 @@
 package com.git.test;
 
 public class UW {
-	
+	public static void main(String[] args) {
+		System.out.println("In UW");
+	}
 }
